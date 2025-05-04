e9ccb93ca46ffe6d94b226f2bdec1b76654da189
hanhosung <winsomed96@naver.com>
2025-05-04 14:20:31 +0900

    fe: Product 기본 CRUD 코드

--- database/src/main/java/module/database/repository/ProductRepository.java
+++ database/src/main/java/module/database/repository/ProductRepository.java
@@ -3,9 +3,13 @@ package module.database.repository;
 
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import module.database.entity.Monitor;
 import module.database.entity.Product;
+import module.database.entity.ProductSize;
 import module.database.entity.QProduct;
+import module.database.entity.QProductSize;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -15,18 +19,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static module.database.entity.QMonitor.monitor;
 import static module.database.entity.QProduct.product;
+import static module.database.entity.QProductSize.productSize;
 
 @Repository
 @RequiredArgsConstructor
 public class ProductRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
+    private final EntityManager entityManager;
 
-    @Transactional(readOnly = true)
     public Page<Product> getProducts(Pageable pageable) {
-
         List<Product> content = jpaQueryFactory
                 .selectFrom(product)
                 .orderBy(product.id.desc())
@@ -39,6 +45,28 @@ public class ProductRepository {
                 .from(product);
 
         return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+    }
 
+    public Optional<Product> findById(Long id) {
+        Product findProduct = jpaQueryFactory.selectFrom(product).where(product.id.eq(id)).fetchOne();
+        return Optional.ofNullable(findProduct);
+    }
+
+    public void saveAllProductSize(List<ProductSize> productSizes) {
+        productSizes.forEach(entityManager::persist);
+        entityManager.flush();
+    }
+
+    public void deleteAllProductSize(List<Long> deleteIds) {
+        jpaQueryFactory
+                .delete(productSize)
+                .where(productSize.id.in(deleteIds))
+                .execute();
+        entityManager.flush();
+    }
+
+    public void saveProduct(Product product) {
+        entityManager.persist(product);
+        entityManager.flush();
     }
 }
